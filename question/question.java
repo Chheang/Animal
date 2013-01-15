4ccc034944694d9a4c1df9dd99caf25ef11ce4d4
Chheang <chheang.oeng@gmail.com>
2013-01-15 22:08:57 +0700

    question folder modified….

--- question/question.java
+++ question/question.java
@@ -9,24 +9,18 @@ public class question {
     
     public static Scanner s1 = new Scanner(System.in);
     
-//    public static String question(String question){
-//        System.out.println(question);
-//        return s1.nextLine();
-//    }
     public static String question(String question){
         System.out.println(question);
-        String s="n";
-        ActionEvent evt=null;
-        Interface inter = null;
-        addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        s = jButtonActionPerformeds(evt);
-        return s;
+        return s1.nextLine();
     }
+//    public static String question(String question){
+//        System.out.println(question);
+//        String s="n";
+//        ActionEvent evt=null;
+//        Interface inter = null;
+//        s = inter.jButtonActionPerformeds(evt);
+//        return s;
+//    }
     public static boolean questionTT(boolean b1, boolean b2){
         if(b1 & b2){
             return true;
